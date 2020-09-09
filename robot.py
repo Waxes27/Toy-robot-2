a8d2b047d77ff8f33a171e0dfd1e3e74f51b8585
ndumasi <c4r5s3@c4r5s3.co.za>
2020-09-09 17:41:10 +0200

    Toy robot 2

--- robot.py
+++ robot.py
@@ -1,11 +1,11 @@
-direction = True
+
 PosNeg = 1
 
 def name_of_the_robot():
     name = input("What do you want to name your robot? ")
     return name
 
-def command_switcher(command, yaxis, xaxis,coords, name):
+def command_switcher(command, yaxis, xaxis,coords, name, direction):
     if len(command) == 2:
         command, steps = command
         steps = int(steps)
@@ -15,40 +15,95 @@ def command_switcher(command, yaxis, xaxis,coords, name):
             print(" > {} now at position ({})".format(name,coords))
             return
             
-
-    
     if "forward" in command:
         print(" > {} moved forward by {} steps.".format(name,steps))
-        movement(command,yaxis,xaxis,coords,name, steps)
-        return
+        movement(command,yaxis,xaxis,coords,name, steps, direction)
+        return  
     if "back" in command:
         print(" > {} moved back by {} steps.".format(name,steps))
-        movement(command,yaxis,xaxis,coords,name, steps)
+        movement(command,yaxis,xaxis,coords,name, steps, direction)
         return        
     if "left" in command:
-        left(name)
+        direction = left(name, direction, coords, yaxis, xaxis)
+        return direction
+            
+    if "right" in command:
+        direction = right(name, direction, coords, yaxis, xaxis)
+        return direction
             
 
-def left(name):
-    global direction
-    global PosNeg
-    print(direction)
-    if direction is True:
-        direction = False
-        PosNeg = PosNeg*(-1)
-    return PosNeg, direction
+def left(name,direction,coords, yaxis, xaxis):
+    coord = (str(xaxis)+","+str(yaxis))
+    print(" > {} turned left.".format(name))
+    print(" > {} now at position {}.".format(name,coords))
+    if direction == "north":
+        direction = "west"
+    elif direction == "west":
+        direction = "south"
+    elif direction =="south":
+        direction = "east"
+    elif direction == "east":
+        direction = "north"
+    return direction
+
+def right(name, direction, coords, yaxis, xaxis):
+    coord = (str(xaxis)+","+str(yaxis))
+    print(" > {} turned right.".format(name))
+    print(" > {} now at position ({}).".format(name, coord))
+    if direction == "north":
+        direction = "east"
+    elif direction == "west":
+        direction = "north"
+    elif direction =="south":
+        direction = "west"
+    elif direction == "east":
+        direction = "south"
+    return direction
 
 
-def movement(command, yaxis, xaxis,coords,name,steps):
-    if "forward" in command and direction is True:
+def movement(command, yaxis, xaxis,coords,name,steps,direction):
+    #direction = "east"
+    if "forward" in command and direction == "north":
         yaxis = yaxis + steps
-        coords = (str(xaxis)+","+str(yaxis))
-        print(" > {} now at position ({}).".format(name, coords))
-    elif "back" in command and direction is True:
-        yaxis = yaxis - steps
-        coords = (str(xaxis)+","+str(yaxis))
-        print(" > {} now at position ({}).".format(name, coords))
+        coord = (str(xaxis)+","+str(yaxis))
+        print(" > {} now at position ({}).".format(name, coord))
 
+    if "forward" in command and direction == "east":
+        xaxis = xaxis + steps
+        coord = (str(xaxis)+","+str(yaxis))
+        print(" > {} now at position ({}).".format(name, coord))
+
+    if "forward" in command and direction == "south":
+        yaxis = yaxis - steps
+        coord = (str(xaxis)+","+str(yaxis))
+        print(" > {} now at position ({}).".format(name, coord))
+    
+    if "forward" in command and direction == "west":
+        xaxis = xaxis - steps
+        coord = (str(xaxis)+","+str(yaxis))
+        print(" > {} now at position ({}).".format(name, coord))
+    
+
+
+    if "back" in command and direction == "north":
+        yaxis = yaxis - steps
+        coord = (str(xaxis)+","+str(yaxis))
+        print(" > {} now at position ({}).".format(name, coord))
+
+    if "back" in command and direction == "east":
+        xaxis = xaxis - steps
+        coord = (str(xaxis)+","+str(yaxis))
+        print(" > {} now at position ({}).".format(name, coord))
+
+    if "back" in command and direction == "south":
+        yaxis = yaxis + steps
+        coord = (str(xaxis)+","+str(yaxis))
+        print(" > {} now at position ({}).".format(name, coord))
+    
+    if "back" in command and direction == "west":
+        xaxis = xaxis + steps
+        coord = (str(xaxis)+","+str(yaxis))
+        print(" > {} now at position ({}).".format(name, coord))
 
 
 def help_():
@@ -75,8 +130,8 @@ def sorry(name, command, commands, command1):
         print("{}: Sorry, I did not understand '{}'.".format(name,command1))
         
 
-
 def robot_start():
+    direction = "north"
     xaxis = 0
     yaxis = 0
     coords = (xaxis, yaxis)
@@ -104,14 +159,27 @@ def robot_start():
             continue
         
         elif "forward" in command:
-            command_switcher(command, yaxis, xaxis, coords, name)
+            command_switcher(command, yaxis, xaxis, coords, name, direction)
             command = user_command(name)
             continue
         
         elif "back" in command:
-            command_switcher(command, yaxis, xaxis, coords, name)
+            command_switcher(command, yaxis, xaxis, coords, name, direction)
             command = user_command(name)
             continue
+
+        elif "left" in command:
+            direction = left(name, direction, coords, yaxis,xaxis)
+            command_switcher(command, yaxis, xaxis, coords, name, direction)
+            command = user_command(name)
+            continue
+
+        elif "right" in command:
+            direction = right(name, direction, coords, yaxis,xaxis)
+            #command_switcher(command, yaxis, xaxis, coords, name, direction)
+            command = user_command(name)
+            continue
+
 
         break
             
